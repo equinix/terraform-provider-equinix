6dced13eda2504bcb81b01cead4e4ae100741784
Tomas Karasek <tokarasek@gmail.com>
2019-10-23 23:42:02 +0300

    fix imports for plugin-sdk in project datasource

--- packet/datasource_packet_project.go
+++ packet/datasource_packet_project.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"path"
 
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/packethost/packngo"
 )
 
