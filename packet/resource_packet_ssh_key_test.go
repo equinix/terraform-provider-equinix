6b4e25d58cee2b024268c015e75753a168aa781f
Tomas Karasek <tom.to.the.k@gmail.com>
2019-10-30 17:28:00 +0200

    implement import of Project SSH keys

--- packet/resource_packet_ssh_key_test.go
+++ packet/resource_packet_ssh_key_test.go
@@ -31,6 +31,33 @@ func TestAccPacketSSHKey_Basic(t *testing.T) {
 						"packet_ssh_key.foobar", "name", fmt.Sprintf("foobar-%d", rInt)),
 					resource.TestCheckResourceAttr(
 						"packet_ssh_key.foobar", "public_key", publicKeyMaterial),
+					resource.TestCheckResourceAttrSet(
+						"packet_ssh_key.foobar", "owner_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccPacketSSHKey_ProjectBasic(t *testing.T) {
+	rInt := acctest.RandInt()
+	publicKeyMaterial, _, err := acctest.RandSSHKeyPair("")
+	if err != nil {
+		t.Fatalf("Cannot generate test SSH key pair: %s", err)
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPacketSSHKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPacketSSHKeyConfig_projectBasic(rInt, publicKeyMaterial),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(
+						"packet_project.test", "id",
+						"packet_project_ssh_key.foobar", "project_id",
+					),
 				),
 			},
 		},
@@ -69,6 +96,28 @@ func TestAccPacketSSHKey_Update(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"packet_ssh_key.foobar", "public_key", publicKeyMaterial),
 				),
+			},
+		},
+	})
+}
+
+func TestAccPacketSSHKey_projectImportBasic(t *testing.T) {
+	sshKey, _, err := acctest.RandSSHKeyPair("")
+	if err != nil {
+		t.Fatalf("Cannot generate test SSH key pair: %s", err)
+	}
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPacketSSHKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPacketSSHKeyConfig_projectBasic(acctest.RandInt(), sshKey),
+			},
+			{
+				ResourceName:      "packet_project_ssh_key.foobar",
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -144,4 +193,18 @@ resource "packet_ssh_key" "foobar" {
     name = "foobar-%d"
     public_key = "%s"
 }`, rInt, publicSshKey)
+}
+
+func testAccCheckPacketSSHKeyConfig_projectBasic(rInt int, publicSshKey string) string {
+	return fmt.Sprintf(`
+
+resource "packet_project" "test" {
+    name = "test-%d"
+}
+
+resource "packet_project_ssh_key" "foobar" {
+    name = "foobar-%d"
+    public_key = "%s"
+	project_id = packet_project.test.id
+}`, rInt, rInt, publicSshKey)
 }
