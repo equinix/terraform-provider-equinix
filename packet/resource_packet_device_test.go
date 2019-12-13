291c4d7713ead9790c45d2d21a8bd80dc9fe1237
Tomas Karasek <tom.to.the.k@gmail.com>
2019-12-13 19:08:35 +0200

    Add sweep for packet_device

--- packet/resource_packet_device_test.go
+++ packet/resource_packet_device_test.go
@@ -2,8 +2,10 @@ package packet
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -11,6 +13,54 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/packethost/packngo"
 )
+
+func init() {
+	resource.AddTestSweepers("packet_device", &resource.Sweeper{
+		Name: "packet_device",
+		F:    testSweepDevices,
+	})
+}
+
+func testSweepDevices(region string) error {
+	log.Printf("[DEBUG] Sweeping devices")
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return fmt.Errorf("Error getting client for sweeping devices: %s", err)
+	}
+	client := meta.(*packngo.Client)
+
+	ps, _, err := client.Projects.List(nil)
+	if err != nil {
+		return fmt.Errorf("Error getting project list for sweepeing devices: %s", err)
+	}
+	pids := []string{}
+	for _, p := range ps {
+		if strings.HasPrefix(p.Name, "tfacc-") {
+			pids = append(pids, p.ID)
+		}
+	}
+	dids := []string{}
+	for _, pid := range pids {
+		ds, _, err := client.Devices.List(pid, nil)
+		if err != nil {
+			return fmt.Errorf("Error listing devices to sweep: %s", err)
+		}
+		for _, d := range ds {
+			if strings.HasPrefix(d.Hostname, "tfacc-") {
+				dids = append(dids, d.ID)
+			}
+		}
+	}
+
+	for _, did := range dids {
+		log.Printf("Removing device %s", did)
+		_, err := client.Devices.Delete(did, true)
+		if err != nil {
+			return fmt.Errorf("Error deleting device %s", err)
+		}
+	}
+	return nil
+}
 
 // Regexp vars for use with resource.ExpectError
 var matchErrMustBeProvided = regexp.MustCompile(".* must be provided when .*")
