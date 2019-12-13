9265860cd7e3d49dd3ab1ebe44051a419c70d5bf
Tomas Karasek <tom.to.the.k@gmail.com>
2019-12-13 19:32:28 +0200

    Add sweeper for projects

--- packet/resource_packet_project_test.go
+++ packet/resource_packet_project_test.go
@@ -2,7 +2,9 @@ package packet
 
 import (
 	"fmt"
+	"log"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -10,6 +12,42 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/packethost/packngo"
 )
+
+func init() {
+	resource.AddTestSweepers("packet_project", &resource.Sweeper{
+		Name:         "packet_project",
+		Dependencies: []string{"packet_device"},
+		F:            testSweepProjects,
+	})
+}
+
+func testSweepProjects(region string) error {
+	log.Printf("[DEBUG] Sweeping projects")
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return fmt.Errorf("Error getting client for sweeping projects: %s", err)
+	}
+	client := meta.(*packngo.Client)
+
+	ps, _, err := client.Projects.List(nil)
+	if err != nil {
+		return fmt.Errorf("Error getting project list for sweepeing projects: %s", err)
+	}
+	pids := []string{}
+	for _, p := range ps {
+		if strings.HasPrefix(p.Name, "tfacc-") {
+			pids = append(pids, p.ID)
+		}
+	}
+	for _, pid := range pids {
+		log.Printf("Removing project %s", pid)
+		_, err := client.Projects.Delete(pid)
+		if err != nil {
+			return fmt.Errorf("Error deleting project %s", err)
+		}
+	}
+	return nil
+}
 
 func TestAccPacketProject_Basic(t *testing.T) {
 	var project packngo.Project
