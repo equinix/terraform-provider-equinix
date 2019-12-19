db15fcb905969445324ddc9b2c3bd6a25ebec55f
Tomas Karasek <tom.to.the.k@gmail.com>
2019-12-19 13:00:27 +0200

    Add sweeper for volumes

--- packet/resource_packet_volume_test.go
+++ packet/resource_packet_volume_test.go
@@ -2,6 +2,8 @@ package packet
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -9,6 +11,52 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/packethost/packngo"
 )
+
+func init() {
+	resource.AddTestSweepers("packet_volume", &resource.Sweeper{
+		Name: "packet_volume",
+		F:    testSweepVolumes,
+	})
+}
+
+func testSweepVolumes(region string) error {
+	log.Printf("[DEBUG] Sweeping volumes")
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return fmt.Errorf("Error getting client for sweeping volumes: %s", err)
+	}
+	client := meta.(*packngo.Client)
+
+	ps, _, err := client.Projects.List(nil)
+	if err != nil {
+		return fmt.Errorf("Error getting project list for sweepeing volumes: %s", err)
+	}
+	pids := []string{}
+	for _, p := range ps {
+		if strings.HasPrefix(p.Name, "tfacc-") {
+			pids = append(pids, p.ID)
+		}
+	}
+	vids := []string{}
+	for _, pid := range pids {
+		vs, _, err := client.Volumes.List(pid, nil)
+		if err != nil {
+			return fmt.Errorf("Error listing volumes to sweep: %s", err)
+		}
+		for _, v := range vs {
+			vids = append(vids, v.ID)
+		}
+	}
+
+	for _, vid := range vids {
+		log.Printf("Removing volume %s", vid)
+		_, err := client.Volumes.Delete(vid)
+		if err != nil {
+			return fmt.Errorf("Error deleting volume %s", err)
+		}
+	}
+	return nil
+}
 
 func TestAccPacketVolume_Basic(t *testing.T) {
 	var volume packngo.Volume
