e4ea2d9250c537171926af3562c8a4d05a4dc824
Marques Johansson <mjohansson@equinix.com>
2021-04-09 18:37:58 -0400

    add metro to datasource metal_precreate_ip_block
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- metal/datasource_metal_precreated_ip_block.go
+++ metal/datasource_metal_precreated_ip_block.go
@@ -10,30 +10,43 @@ import (
 func dataSourceMetalPreCreatedIPBlock() *schema.Resource {
 	s := metalIPComputedFields()
 	s["project_id"] = &schema.Schema{
-		Type:     schema.TypeString,
-		Required: true,
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "ID of the project where the searched block should be.",
 	}
 	s["global"] = &schema.Schema{
-		Type:     schema.TypeBool,
-		Optional: true,
-		Default:  false,
+		Type:        schema.TypeBool,
+		Optional:    true,
+		Default:     false,
+		Description: "Whether to look for global block. Default is false for backward compatibility.",
 	}
 	s["public"] = &schema.Schema{
-		Type:     schema.TypeBool,
-		Required: true,
+		Type:        schema.TypeBool,
+		Required:    true,
+		Description: "Whether to look for public or private block.",
 	}
 
 	s["facility"] = &schema.Schema{
-		Type:     schema.TypeString,
-		Optional: true,
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Facility of the searched block. (for non-global blocks).",
 	}
+
+	s["metro"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Metro of the searched block (for non-global blocks).",
+	}
+
 	s["address_family"] = &schema.Schema{
-		Type:     schema.TypeInt,
-		Required: true,
+		Type:        schema.TypeInt,
+		Required:    true,
+		Description: "4 or 6, depending on which block you are looking for.",
 	}
 	s["cidr_notation"] = &schema.Schema{
-		Type:     schema.TypeString,
-		Computed: true,
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "CIDR notation of the looked up block.",
 	}
 	s["quantity"] = &schema.Schema{
 		Type:     schema.TypeInt,
@@ -66,8 +79,13 @@ func dataSourceMetalReservedIPBlockRead(d *schema.ResourceData, meta interface{}
 	}
 
 	fval, fok := d.GetOk("facility")
-	if fok && global {
-		return fmt.Errorf("You can't specify facility for global IP block - addresses from global blocks can be assigned to devices across several facilities")
+	mval, mok := d.GetOk("metro")
+	if (fok || mok) && global {
+		return fmt.Errorf("You can't specify facility for global IP block - addresses from global blocks can be assigned to devices across several locations")
+	}
+
+	if fok && mok {
+		return fmt.Errorf("You can't specify both facility and metro.")
 	}
 
 	if fok {
@@ -75,6 +93,17 @@ func dataSourceMetalReservedIPBlockRead(d *schema.ResourceData, meta interface{}
 		facility := fval.(string)
 		for _, ip := range ips {
 			if ip.Public == public && ip.AddressFamily == ipv && facility == ip.Facility.Code {
+				if err := loadBlock(d, &ip); err != nil {
+					return err
+				}
+				break
+			}
+		}
+	} else if mok {
+		// lookup of not-global block
+		metro := mval.(string)
+		for _, ip := range ips {
+			if ip.Public == public && ip.AddressFamily == ipv && metro == ip.Metro.Code {
 				if err := loadBlock(d, &ip); err != nil {
 					return err
 				}
