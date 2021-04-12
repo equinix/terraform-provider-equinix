784cedd990f11992d18a1be6454a24ef7a432ab2
Tomas Karasek <tom.to.the.k@gmail.com>
2021-04-12 18:10:58 +0300

    Add test attching IP to a device in a metro
    Signed-off-by: Tomas Karasek <tom.to.the.k@gmail.com>

--- metal/resource_metal_ip_attachment_test.go
+++ metal/resource_metal_ip_attachment_test.go
@@ -62,7 +62,7 @@ resource "metal_project" "test" {
 resource "metal_device" "test" {
   hostname         = "tfacc-device-ip-attachment-test"
   plan             = "t1.small.x86"
-  facilities       = ["ewr1"]
+  facilities       = ["sv15"]
   operating_system = "ubuntu_16_04"
   billing_cycle    = "hourly"
   project_id       = "${metal_project.test.id}"
@@ -70,7 +70,63 @@ resource "metal_device" "test" {
 
 resource "metal_reserved_ip_block" "test" {
     project_id = "${metal_project.test.id}"
-    facility = "ewr1"
+    facility = "sv15"
+	quantity = 2
+}
+
+
+resource "metal_ip_attachment" "test" {
+	device_id = "${metal_device.test.id}"
+	cidr_notation = "${cidrhost(metal_reserved_ip_block.test.cidr_notation,0)}/32"
+}`, name)
+}
+
+func TestAccMetalIPAttachment_Metro(t *testing.T) {
+
+	rs := acctest.RandString(10)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckMetalIPAttachmentDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckMetalIPAttachmentConfig_Metro(rs),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"metal_ip_attachment.test", "public", "true"),
+					resource.TestCheckResourceAttrPair(
+						"metal_ip_attachment.test", "device_id",
+						"metal_device.test", "id"),
+				),
+			},
+			{
+				ResourceName:      "metal_ip_attachment.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckMetalIPAttachmentConfig_Metro(name string) string {
+	return fmt.Sprintf(`
+resource "metal_project" "test" {
+    name = "tfacc-ip_attachment-%s"
+}
+
+resource "metal_device" "test" {
+  hostname         = "tfacc-device-ip-attachment-test"
+  plan             = "c3.medium.x86"
+  metro            = "sv"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${metal_project.test.id}"
+}
+
+resource "metal_reserved_ip_block" "test" {
+    project_id = "${metal_project.test.id}"
+    metro      = "sv"
 	quantity = 2
 }
 
