b0531297c6f698d160ed949830df23f01b53c422
Marques Johansson <mjohansson@equinix.com>
2021-05-07 15:37:49 -0400

    add missing attributes to metal_spot_market_request datasource
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- metal/datasource_metal_spot_market_request.go
+++ metal/datasource_metal_spot_market_request.go
@@ -1,7 +1,7 @@
 package metal
 
 import (
-	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/packethost/packngo"
@@ -13,13 +13,56 @@ func dataSourceMetalSpotMarketRequest() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"request_id": {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Description: "The id of the Spot Market Request",
+				Required:    true,
 			},
 			"device_ids": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
+				Type:        schema.TypeList,
+				Description: "List of IDs of devices spawned by the referenced Spot Market Request",
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			"devices_min": {
+				Type:        schema.TypeInt,
+				Description: "Miniumum number devices to be created",
+				Computed:    true,
+			},
+			"devices_max": {
+				Type:        schema.TypeInt,
+				Description: "Maximum number devices to be created",
+				Computed:    true,
+			},
+			"max_bid_price": {
+				Type:        schema.TypeFloat,
+				Description: "Maximum price user is willing to pay per hour per device",
+				Computed:    true,
+			},
+			"facilities": {
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Description: "Facility IDs where devices should be created",
+				Computed:    true,
+			},
+			"metro": {
+				Type:        schema.TypeString,
+				Description: "Metro where devices should be created.",
+				Computed:    true,
+			},
+			"project_id": {
+				Type:        schema.TypeString,
+				Description: "Project ID",
+				Computed:    true,
+			},
+			"plan": {
+				Type:        schema.TypeString,
+				Description: "The device plan slug.",
+				Computed:    true,
+			},
+			"end_at": {
+				Type:        schema.TypeString,
+				Description: "Date and time When the spot market request will be ended.",
+				Computed:    true,
 			},
 		},
 		Timeouts: resourceDefaultTimeouts,
@@ -42,8 +85,33 @@ func dataSourceMetalSpotMarketRequestRead(d *schema.ResourceData, meta interface
 	deviceIDs := make([]string, len(smr.Devices))
 	for i, d := range smr.Devices {
 		deviceIDs[i] = d.ID
+
 	}
-	d.Set("device_ids", deviceIDs)
-	d.SetId(id + strings.Join(deviceIDs, "-"))
-	return nil
+
+	facs := smr.Facilities
+	facCodes := []string{}
+
+	for _, f := range facs {
+		facCodes = append(facCodes, f.Code)
+	}
+
+	d.SetId(id)
+
+	return setMap(d, map[string]interface{}{
+		"device_ids": deviceIDs,
+		"end_at": func(d *schema.ResourceData, k string) error {
+			if smr.EndAt != nil {
+				return d.Set(k, smr.EndAt.Format(time.RFC3339))
+			}
+			return nil
+		},
+		"devices_max":   smr.DevicesMax,
+		"devices_min":   smr.DevicesMin,
+		"facilities":    facCodes,
+		"metro":         smr.Metro,
+		"max_bid_price": smr.MaxBidPrice,
+		"plan":          smr.Plan.Slug,
+		"project_id":    smr.Project.ID,
+		// TODO: created_at is not in packngo
+	})
 }
