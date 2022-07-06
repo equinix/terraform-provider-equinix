5e1db536476df8cb16864999764249a322fdc5cd
Srinivasa Yedluri <syedluri@equinix.com>
2022-07-05 17:32:55 -0700

    Review Comments - Retry implementation
    Review Comments - Retry implementation

--- equinix/resource_fabric_connection.go
+++ equinix/resource_fabric_connection.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	v4 "github.com/equinix-labs/fabric-go/fabric/v4"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"log"
 	"time"
@@ -15,7 +16,7 @@ func resourceFabricConnection() *schema.Resource {
 	return &schema.Resource{
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(6 * time.Minute),
-			Update: schema.DefaultTimeout(6 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
 			Delete: schema.DefaultTimeout(6 * time.Minute),
 			Read:   schema.DefaultTimeout(6 * time.Minute),
 		},
@@ -35,7 +36,6 @@ func resourceFabricConnection() *schema.Resource {
 func resourceFabricConnectionCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	log.Printf(" inside fabric connection create resource ")
 	client := meta.(*Config).fabricClient
-
 	ctx = context.WithValue(ctx, v4.ContextAccessToken, meta.(*Config).FabricAuthToken)
 	conType := v4.ConnectionType(d.Get("type").(string))
 	schemaNotifications := d.Get("notifications").([]interface{})
@@ -60,7 +60,6 @@ func resourceFabricConnectionCreate(ctx context.Context, d *schema.ResourceData,
 			mappedServiceToken := serviceTokenToFabric(serviceTokenRequest)
 			connectionASide = v4.ConnectionSide{ServiceToken: &mappedServiceToken}
 		}
-
 		if len(additionalInfoRequest) != 0 {
 			mappedAdditionalInfo := additionalInfoToFabric(additionalInfoRequest)
 			connectionASide = v4.ConnectionSide{AdditionalInfo: mappedAdditionalInfo}
@@ -74,17 +73,14 @@ func resourceFabricConnectionCreate(ctx context.Context, d *schema.ResourceData,
 		accessPoint := zsideMap["access_point"].(interface{}).(*schema.Set).List()
 		serviceTokenRequest := zsideMap["service_token"].(interface{}).(*schema.Set).List()
 		additionalInfoRequest := zsideMap["additional_info"].([]interface{})
-
 		if len(accessPoint) != 0 {
 			ap := accessPointToFabric(accessPoint)
 			connectionZSide = v4.ConnectionSide{AccessPoint: &ap}
 		}
-
 		if len(serviceTokenRequest) != 0 {
 			mappedServiceToken := serviceTokenToFabric(serviceTokenRequest)
 			connectionZSide = v4.ConnectionSide{ServiceToken: &mappedServiceToken}
 		}
-
 		if len(additionalInfoRequest) != 0 {
 			mappedAdditionalInfo := additionalInfoToFabric(additionalInfoRequest)
 			connectionZSide = v4.ConnectionSide{AdditionalInfo: mappedAdditionalInfo}
@@ -115,7 +111,7 @@ func resourceFabricConnectionRead(ctx context.Context, d *schema.ResourceData, m
 	client := meta.(*Config).fabricClient
 	ctx = context.WithValue(ctx, v4.ContextAccessToken, meta.(*Config).FabricAuthToken)
 	conn, _, err := client.ConnectionsApi.GetConnectionByUuid(ctx, d.Id(), nil)
-	//TODO check for 404 or 403 from the API and see if we need specific handling
+
 	if err != nil {
 		log.Printf("[WARN] Connection %s not found ", d.Id())
 		d.SetId("")
@@ -127,59 +123,45 @@ func resourceFabricConnectionRead(ctx context.Context, d *schema.ResourceData, m
 
 func setFabricMap(d *schema.ResourceData, conn v4.Connection) diag.Diagnostics {
 	diags := diag.Diagnostics{}
-
 	err := setMap(d, map[string]interface{}{
-		"name":          conn.Name,
-		"bandwidth":     conn.Bandwidth,
-		"href":          conn.Href,
-		"description":   conn.Description,
-		"is_remote":     conn.IsRemote,
-		"type":          conn.Type_,
-		"state":         conn.State,
-		"direction":     conn.Direction,
-		"operation":     operationToTerra(conn.Operation),
-		"order":         orderMappingToTerra(conn.Order),
-		"change_log":    changeLogToTerra(conn.ChangeLog),
-		"redundancy":    redundancyToTerra(conn.Redundancy),
-		"notifications": notificationToTerra(conn.Notifications),
-		"account":       accountToTerra(conn.Account),
-		"a_side":        connectionSideToTerra(conn.ASide),
-		"z_side":        connectionSideToTerra(conn.ZSide),
+		"name":            conn.Name,
+		"bandwidth":       conn.Bandwidth,
+		"href":            conn.Href,
+		"description":     conn.Description,
+		"is_remote":       conn.IsRemote,
+		"type":            conn.Type_,
+		"state":           conn.State,
+		"direction":       conn.Direction,
+		"operation":       operationToTerra(conn.Operation),
+		"order":           orderMappingToTerra(conn.Order),
+		"change_log":      changeLogToTerra(conn.ChangeLog),
+		"redundancy":      redundancyToTerra(conn.Redundancy),
+		"notifications":   notificationToTerra(conn.Notifications),
+		"account":         accountToTerra(conn.Account),
+		"a_side":          connectionSideToTerra(conn.ASide),
+		"z_side":          connectionSideToTerra(conn.ZSide),
+		"additional_info": additionalInfoToTerra(conn.AdditionalInfo),
 	})
-
 	if err != nil {
 		return diag.FromErr(err)
 	}
-
 	return diags
 }
 
 func resourceFabricConnectionUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	log.Printf(" inside fabric connection update resource ")
 	client := meta.(*Config).fabricClient
 	ctx = context.WithValue(ctx, v4.ContextAccessToken, meta.(*Config).FabricAuthToken)
 	uuid := d.Id()
 	if uuid == "" {
-		//TODO added an error and return
 		return diag.Errorf("No connection found for the value uuid %v ", uuid)
 	}
-
 	dbConn := v4.Connection{}
 	var err error
-	counter := 0
-	//TODO Terraform retry helpers to use
-	for dbConn.State == nil || "ACTIVE" != *dbConn.State {
-		time.Sleep(30 * time.Second)
-		dbConn, _, err = client.ConnectionsApi.GetConnectionByUuid(ctx, uuid, nil)
-		if err != nil {
-			//TODO handle error and add to the diag rather returning it directly
-			return diag.Errorf(" Error while fetching connection for uuid %s and error %v", uuid, err)
-		}
-		if counter >= 4 {
-			break
-		}
-		counter++
+	dbConn, err = waitUntilConnectionIsActive(uuid, meta, ctx)
+	if err != nil {
+		return diag.Errorf(" Either timed out or errored out while fetching connection for uuid %s and error %v", uuid, err)
 	}
-
 	update, err := getUpdateRequest(dbConn, d)
 	if err != nil {
 		return diag.FromErr(err)
@@ -187,21 +169,79 @@ func resourceFabricConnectionUpdate(ctx context.Context, d *schema.ResourceData,
 	updates := []v4.ConnectionChangeOperation{update}
 	_, res, err := client.ConnectionsApi.UpdateConnectionByUuid(ctx, updates, uuid)
 	if err != nil {
-		//TODO handle response
-		fmt.Errorf(" Error response for the connection update, response %v, error %v", res, err)
-		return diag.FromErr(err)
+		return diag.FromErr(fmt.Errorf(" Error response for the connection update, response %v, error %v", res, err))
+	}
+	updatedConn := v4.Connection{}
+	updatedConn, err = waitForConnectionUpdateCompletion(uuid, meta, ctx)
+
+	if err != nil {
+		return diag.FromErr(fmt.Errorf(" Errored while waiting for successful connection update, response %v, error %v", res, err))
 	}
 
-	time.Sleep(1 * time.Minute)
-	updatedConn, _, err := client.ConnectionsApi.GetConnectionByUuid(ctx, uuid, nil)
-	log.Printf(" inside fabric connection update resource ")
-	if err != nil {
-		//TODO handle response
-		fmt.Errorf(" Error response while getting updated conection, response %v, error %v", res, err)
-		return diag.FromErr(err)
-	}
 	d.SetId(updatedConn.Uuid)
 	return setFabricMap(d, updatedConn)
+}
+
+func waitForConnectionUpdateCompletion(uuid string, meta interface{}, ctx context.Context) (v4.Connection, error) {
+	log.Println("Waiting for connection update to complete")
+	stateConf := &resource.StateChangeConf{
+		Target: []string{"COMPLETED"},
+		Refresh: func() (interface{}, string, error) {
+			client := meta.(*Config).fabricClient
+			dbConn, _, err := client.ConnectionsApi.GetConnectionByUuid(ctx, uuid, nil)
+			if err != nil {
+				//TODO handle error and add to the diag rather returning it directly
+				return "", "", err
+			}
+			updatableState := ""
+			if "COMPLETED" == dbConn.Change.Status {
+				updatableState = dbConn.Change.Status
+			}
+			return dbConn, updatableState, nil
+		},
+		Timeout:    2 * time.Minute,
+		Delay:      30 * time.Second,
+		MinTimeout: 30 * time.Second,
+	}
+
+	inter, err := stateConf.WaitForStateContext(ctx)
+	dbConn := v4.Connection{}
+
+	if err == nil {
+		dbConn = inter.(v4.Connection)
+	}
+	return dbConn, err
+}
+
+func waitUntilConnectionIsActive(uuid string, meta interface{}, ctx context.Context) (v4.Connection, error) {
+	log.Println("Waiting for connection to be in active state ")
+	stateConf := &resource.StateChangeConf{
+		Target: []string{"ACTIVE"},
+		Refresh: func() (interface{}, string, error) {
+			client := meta.(*Config).fabricClient
+			dbConn, _, err := client.ConnectionsApi.GetConnectionByUuid(ctx, uuid, nil)
+
+			if err != nil {
+				return "", "", err
+			}
+			updatableState := ""
+			if "ACTIVE" == *dbConn.State {
+				updatableState = string(*dbConn.State)
+			}
+			return dbConn, updatableState, nil
+		},
+		Timeout:    5 * time.Minute,
+		Delay:      30 * time.Second,
+		MinTimeout: 30 * time.Second,
+	}
+
+	inter, err := stateConf.WaitForStateContext(ctx)
+	dbConn := v4.Connection{}
+
+	if err == nil {
+		dbConn = inter.(v4.Connection)
+	}
+	return dbConn, err
 }
 
 func resourceFabricConnectionDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -211,15 +251,12 @@ func resourceFabricConnectionDelete(ctx context.Context, d *schema.ResourceData,
 	ctx = context.WithValue(ctx, v4.ContextAccessToken, meta.(*Config).FabricAuthToken)
 	uuid := d.Id()
 	if uuid == "" {
-		//TODO added an error and return
 		return diag.Errorf("No uuid found %v ", uuid)
 	}
 	_, resp, err := client.ConnectionsApi.DeleteConnectionByUuid(ctx, uuid)
-	//TODO check if delete is returing 404 in case if API did not find an active connction for the ID
 	if err != nil {
 		fmt.Errorf(" Error response for the connection delete error %v and response %v", err, resp)
 		return diag.FromErr(err)
 	}
-
 	return diags
 }
