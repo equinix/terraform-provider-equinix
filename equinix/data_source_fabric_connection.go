9bdcc608a7c37640a557abccfe00893a22b8d253
Srinivasa Yedluri <syedluri@equinix.com>
2022-07-07 15:30:15 -0700

    PR Comments
    PR Comments

--- equinix/data_source_fabric_connection.go
+++ equinix/data_source_fabric_connection.go
@@ -2,7 +2,6 @@ package equinix
 
 import (
 	"context"
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -16,7 +15,6 @@ func dataSourceFabricConnection() *schema.Resource {
 }
 
 func dataSourceFabricConnectionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	fmt.Println(" inside fabric connection read data ")
 	uuid, _ := d.Get("uuid").(string)
 	d.SetId(uuid)
 	return resourceFabricConnectionRead(ctx, d, meta)
