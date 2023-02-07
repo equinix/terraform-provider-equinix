62bbb91b973694f78f990ecd140896111a3a301d
Rui Ling <rling@equinix.com>
2023-02-06 17:56:19 -0800

    NFV-23346, NFV-23348: introduce new file resource, add unit test cases and acceptance tests for file resource

---
+++ equinix/resource_network_file.go
@@ -0,0 +1,207 @@
+package equinix
+
+import (
+	"context"
+	"fmt"
+	"github.com/equinix/ne-go"
+	"github.com/equinix/rest-go"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"net/http"
+	"strings"
+)
+
+var networkFileSchemaNames = map[string]string{
+	"UUID":           "uuid",
+	"FileName":       "file_name",
+	"Content":        "content",
+	"MetroCode":      "metro_code",
+	"DeviceTypeCode": "device_type_code",
+	"ProcessType":    "process_type",
+	"IsSelfManaged":  "self_managed",
+	"IsBYOL":         "byol",
+	"Status":         "status",
+}
+
+var networkFileDescriptions = map[string]string{
+	"UUID":           "Unique identifier of file resource",
+	"FileName":       "File name",
+	"Content":        "Uploaded file content, expected to be a UTF-8 encoded string",
+	"MetroCode":      "File upload location metro code",
+	"DeviceTypeCode": "Device type code",
+	"ProcessType":    "File process type (LICENSE or CLOUD_INIT)",
+	"IsSelfManaged":  "Boolean value that determines device management mode: self-managed (default) or equinix-managed",
+	"IsBYOL":         "Boolean value that determines device licensing mode: bring your own license (default) or subscription",
+	"Status":         "File upload status",
+}
+
+func resourceNetworkFile() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceNetworkFileCreate,
+		ReadContext:   resourceNetworkFileRead,
+		DeleteContext: resourceNetworkFileDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema:      createNetworkFileSchema(),
+		Description: "Resource allows creation and management of Equinix Network Edge device files",
+	}
+}
+
+func createNetworkFileSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		networkFileSchemaNames["UUID"]: {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: networkFileDescriptions["UUID"],
+		},
+		networkFileSchemaNames["FileName"]: {
+			Type:        schema.TypeString,
+			Required:    true,
+			ForceNew:    true,
+			Description: networkFileDescriptions["FileName"],
+		},
+		networkFileSchemaNames["Content"]: {
+			Type:        schema.TypeString,
+			Required:    true,
+			ForceNew:    true,
+			Sensitive:   true,
+			Description: networkFileDescriptions["Content"],
+		},
+		networkFileSchemaNames["MetroCode"]: {
+			Type:         schema.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: stringIsMetroCode(),
+			Description:  networkFileDescriptions["MetroCode"],
+		},
+		networkFileSchemaNames["DeviceTypeCode"]: {
+			Type:         schema.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: validation.StringIsNotEmpty,
+			Description:  networkFileDescriptions["DeviceTypeCode"],
+		},
+		networkFileSchemaNames["ProcessType"]: {
+			Type:         schema.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: validation.StringInSlice([]string{"LICENSE", "CLOUD_INIT"}, false),
+			Description:  networkFileDescriptions["ProcessType"],
+		},
+		networkFileSchemaNames["IsSelfManaged"]: {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     true,
+			ForceNew:    true,
+			Description: networkFileDescriptions["IsSelfManaged"],
+		},
+		networkFileSchemaNames["IsBYOL"]: {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Default:     true,
+			ForceNew:    true,
+			Description: networkFileDescriptions["IsBYOL"],
+		},
+		networkFileSchemaNames["Status"]: {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: networkFileDescriptions["Status"],
+		},
+	}
+}
+
+func resourceNetworkFileCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Config).ne
+	m.(*Config).addModuleToNEUserAgent(&client, d)
+	var diags diag.Diagnostics
+	fileRequest := createFileRequest(d)
+	uuid, err := client.UploadFile(fileRequest["MetroCode"], fileRequest["DeviceTypeCode"], fileRequest["ProcessType"],
+		fileRequest["DeviceManagementType"], fileRequest["LicenseMode"], fileRequest["FileName"],
+		strings.NewReader(fileRequest["Content"]))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(ne.StringValue(uuid))
+	diags = append(diags, resourceNetworkFileRead(ctx, d, m)...)
+	return diags
+}
+
+func resourceNetworkFileRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Config).ne
+	m.(*Config).addModuleToNEUserAgent(&client, d)
+	var diags diag.Diagnostics
+	file, err := client.GetFile(d.Id())
+	if err != nil {
+		if restErr, ok := err.(rest.Error); ok {
+			if restErr.HTTPCode == http.StatusNotFound {
+				d.SetId("")
+				return diags
+			}
+		}
+		return diag.FromErr(err)
+	}
+	if err := updateFileResource(file, d); err != nil {
+		return diag.FromErr(err)
+	}
+	return diags
+}
+
+func resourceNetworkFileDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func createFileRequest(d *schema.ResourceData) map[string]string {
+	fileRequest := make(map[string]string)
+	if v, ok := d.GetOk(networkFileSchemaNames["FileName"]); ok {
+		fileRequest["FileName"] = v.(string)
+	}
+	if v, ok := d.GetOk(networkFileSchemaNames["Content"]); ok {
+		fileRequest["Content"] = v.(string)
+	}
+	if v, ok := d.GetOk(networkFileSchemaNames["MetroCode"]); ok {
+		fileRequest["MetroCode"] = v.(string)
+	}
+	if v, ok := d.GetOk(networkFileSchemaNames["DeviceTypeCode"]); ok {
+		fileRequest["DeviceTypeCode"] = v.(string)
+	}
+	if v, ok := d.GetOk(networkFileSchemaNames["ProcessType"]); ok {
+		fileRequest["ProcessType"] = v.(string)
+	}
+	isSelfManaged := d.Get(neDeviceSchemaNames["IsSelfManaged"]).(bool)
+	if isSelfManaged {
+		fileRequest["DeviceManagementType"] = ne.DeviceManagementTypeSelf
+	} else {
+		fileRequest["DeviceManagementType"] = ne.DeviceManagementTypeEquinix
+	}
+	isBYOL := d.Get(neDeviceSchemaNames["IsBYOL"]).(bool)
+	if isBYOL {
+		fileRequest["LicenseMode"] = ne.DeviceLicenseModeBYOL
+	} else {
+		fileRequest["LicenseMode"] = ne.DeviceLicenseModeSubscription
+	}
+	return fileRequest
+}
+
+func updateFileResource(file *ne.File, d *schema.ResourceData) error {
+	if err := d.Set(networkFileSchemaNames["UUID"], file.UUID); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["UUID"], err)
+	}
+	if err := d.Set(networkFileSchemaNames["FileName"], file.FileName); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["FileName"], err)
+	}
+	if err := d.Set(networkFileSchemaNames["MetroCode"], file.MetroCode); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["MetroCode"], err)
+	}
+	if err := d.Set(networkFileSchemaNames["DeviceTypeCode"], file.DeviceTypeCode); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["DeviceTypeCode"], err)
+	}
+	if err := d.Set(networkFileSchemaNames["ProcessType"], file.ProcessType); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["ProcessType"], err)
+	}
+	if err := d.Set(networkFileSchemaNames["Status"], file.Status); err != nil {
+		return fmt.Errorf("error reading %s: %s", networkFileSchemaNames["Status"], err)
+	}
+	return nil
+}
