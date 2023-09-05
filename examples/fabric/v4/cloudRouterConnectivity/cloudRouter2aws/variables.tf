60f082c39638374d3a6bea977f5b95f85129d2a9
Manu Bhat <mshankarnarayanabhat@equinix.com>
2023-09-05 16:45:04 -0700

    Add aws and gcp connection workflow code

---
+++ examples/fabric/v4/cloudRouterConnectivity/cloudRouter2aws/variables.tf
@@ -0,0 +1,21 @@
+variable "equinix_client_id" {}
+variable "equinix_client_secret" {}
+
+variable "fcr_uuid" {}
+variable "notifications_type" {}
+variable "notifications_emails" {}
+variable "purchase_order_number" {}
+
+variable "connection_name" {}
+variable "connection_type" {}
+variable "bandwidth" {}
+variable "redundancy" {}
+
+variable "aside_ap_type" {}
+variable "zside_ap_type" {}
+variable "zside_ap_authentication_key" {}
+
+variable "zside_ap_profile_type" {}
+variable "fabric_sp_name" {}
+variable "zside_location" {}
+variable "seller_region" {}
--- examples/fabric/v4/cloudRouterConnectivity/cloudRouter2gcp/terraform.tf
+++ examples/fabric/v4/cloudRouterConnectivity/cloudRouter2gcp/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     equinix = {
-      source="developer.equinix.com/terraform/equinix"
+      source = "developer.equinix.com/terraform/equinix"
       version = "9.0.0"
     }
   }
