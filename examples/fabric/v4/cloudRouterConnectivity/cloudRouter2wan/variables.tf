76dda64da1a21f9d2d25214674d79c1ae25dc6c5
Cindy Bai <74014751+xbai-equinix@users.noreply.github.com>
2023-09-27 09:49:30 -0700

    Add fcr2ipwan connection support

---
+++ examples/fabric/v4/cloudRouterConnectivity/cloudRouter2wan/variables.tf
@@ -0,0 +1,18 @@
+variable "equinix_client_id" {}
+variable "equinix_client_secret" {}
+
+variable "fcr_uuid" {}
+variable "network_uuid" {}
+
+variable "notifications_type" {}
+variable "notifications_emails" {}
+variable "purchase_order_number" {}
+
+variable "connection_name" {}
+variable "connection_type" {}
+variable "bandwidth" {}
+
+variable "aside_ap_type" {}
+variable "zside_ap_type" {}
+
+
