076afaa7f38d1c333b03bec5b8da9ec76cf3d4e1
Charles Treatman <ctreatman@equinix.com>
2023-12-19 14:01:11 -0600

    refactor: move mutexkv to its own package
    Moving mutexkv out of provider.go makes it easier to restructure
    our code.

--- equinix/provider.go
+++ equinix/provider.go
@@ -22,7 +22,6 @@ import (
 )
 
 var (
-	metalMutexKV         = NewMutexKV()
 	DeviceNetworkTypes   = []string{"layer3", "hybrid", "layer2-individual", "layer2-bonded"}
 	DeviceNetworkTypesHB = []string{"layer3", "hybrid", "hybrid-bonded", "layer2-individual", "layer2-bonded"}
 	NetworkTypeList      = strings.Join(DeviceNetworkTypes, ", ")
