2c1068062bb4b565b57995eb6fde1353692ba391
thogarty <139183873+thogarty@users.noreply.github.com>
2024-05-08 12:01:36 -0700

    test: Add tests for Panthers PDS User (#668)
    * Equinix PDS users have different API responses that led to the
    following error:
    https://github.com/equinix/terraform-provider-equinix/issues/657
    * This adds tests and upgrades the version of equinix-sdk-go to account
    for the missing `projectId` property on `project` responses from the API

--- equinix/data_source_fabric_port_acc_test.go
+++ equinix/data_source_fabric_port_acc_test.go
@@ -133,3 +133,93 @@ func testDataSourceFabricPorts(port_name string) string {
 		}`,
 		port_name)
 }
+
+func TestAccDataSourceFabricPort_PPDS(t *testing.T) {
+	ports := GetFabricEnvPorts(t)
+	var port fabricv4.PortResponse
+	var portType, portState, portEncapsulationType, portRedundancyPriority string
+	if len(ports) > 0 {
+		port = ports["ppds"]["dot1q"][0]
+		portType = string(port.GetType())
+		portState = string(port.GetState())
+		portEncapsulation := port.GetEncapsulation()
+		portEncapsulationType = string(portEncapsulation.GetType())
+		portRedundancy := port.GetRedundancy()
+		portRedundancyPriority = string(portRedundancy.GetPriority())
+	}
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t); acceptance.TestAccPreCheckProviderConfigured(t) },
+		ExternalProviders: acceptance.TestExternalProviders,
+		Providers:         acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceFabricPort(port.GetUuid()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "id", port.GetUuid()),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "name", port.GetName()),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_port.test", "bandwidth"),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_port.test", "used_bandwidth"),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "type", portType),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "encapsulation.0.type", portEncapsulationType),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "state", portState),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_port.test", "redundancy.0.priority", portRedundancyPriority),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_port.test", "lag_enabled"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceFabricPorts_PPDS(t *testing.T) {
+	ports := GetFabricEnvPorts(t)
+	var port fabricv4.PortResponse
+	var portType, portState, portEncapsulationType, portRedundancyPriority string
+	if len(ports) > 0 {
+		port = ports["ppds"]["dot1q"][0]
+		portType = string(port.GetType())
+		portState = string(port.GetState())
+		portEncapsulation := port.GetEncapsulation()
+		portEncapsulationType = string(portEncapsulation.GetType())
+		portRedundancy := port.GetRedundancy()
+		portRedundancyPriority = string(portRedundancy.GetPriority())
+	}
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t); acceptance.TestAccPreCheckProviderConfigured(t) },
+		ExternalProviders: acceptance.TestExternalProviders,
+		Providers:         acceptance.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceFabricPorts(port.GetName()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "id", port.GetUuid()),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "data.0.name", port.GetName()),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_ports.test", "data.0.bandwidth"),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_ports.test", "data.0.used_bandwidth"),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "data.0.type", portType),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "data.0.state", portState),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "data.0.encapsulation.0.type", portEncapsulationType),
+					resource.TestCheckResourceAttr(
+						"data.equinix_fabric_ports.test", "data.0.redundancy.0.priority", portRedundancyPriority),
+					resource.TestCheckResourceAttrSet(
+						"data.equinix_fabric_ports.test", "data.0.lag_enabled"),
+				),
+			},
+		},
+	})
+}
