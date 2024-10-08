209a35877ef2e75b474ac7f81da051257e26f3e3
Tim Hogarty <thogarty@equinix.com>
2024-10-08 13:55:09 -0700

    Fix go linting errors

--- internal/resources/fabric/connection_route_filter/resource_test.go
+++ internal/resources/fabric/connection_route_filter/resource_test.go
@@ -3,13 +3,14 @@ package connection_route_filter_test
 import (
 	"context"
 	"fmt"
-	"github.com/equinix/equinix-sdk-go/services/fabricv4"
-	"github.com/equinix/terraform-provider-equinix/internal/fabric/testing_helpers"
 	"testing"
 	"time"
 
 	"github.com/equinix/terraform-provider-equinix/internal/acceptance"
+	"github.com/equinix/terraform-provider-equinix/internal/fabric/testing_helpers"
 	"github.com/equinix/terraform-provider-equinix/internal/resources/fabric/connection_route_filter"
+
+	"github.com/equinix/equinix-sdk-go/services/fabricv4"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
