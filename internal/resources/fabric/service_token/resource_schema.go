74452bc486fe4d4589981d647fe0dc5b4be2e1a0
srushti-patl <srpatel@equinix.com>
2024-10-16 13:43:41 -0700

    feat: Adding Fabric Service Token Resource and Data Source

--- internal/resources/fabric/service_token/resource_schema.go
+++ internal/resources/fabric/service_token/resource_schema.go
@@ -112,14 +112,12 @@ func serviceTokenConnectionSch() *schema.Resource {
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Computed:    true,
-				Default:     true,
 				Description: "Authorization to connect remotely",
 			},
 			"allow_custom_bandwidth": {
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Computed:    true,
-				Default:     true,
 				Description: "Allow custom bandwidth value",
 			},
 			"bandwidth_limit": {
