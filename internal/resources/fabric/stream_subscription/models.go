012270019e00096ee3d01416a81d42be68534633
Tim Hogarty <thogarty@equinix.com>
2025-01-30 17:33:01 -0800

    Schemas and models for stream subscriptions

--- internal/resources/fabric/stream_subscription/models.go
+++ internal/resources/fabric/stream_subscription/models.go
@@ -1,1 +1,343 @@
 package stream_subscription
+
+import (
+	"context"
+
+	"github.com/equinix/terraform-provider-equinix/internal/fabric"
+	int_fw "github.com/equinix/terraform-provider-equinix/internal/framework"
+	fwtypes "github.com/equinix/terraform-provider-equinix/internal/framework/types"
+
+	"github.com/equinix/equinix-sdk-go/services/fabricv4"
+
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+type DataSourceByIdsModel struct {
+	ID             types.String `tfsdk:"id"`
+	StreamID       types.String `tfsdk:"stream_id"`
+	SubscriptionID types.String `tfsdk:"subscription_id"`
+	BaseStreamSubscriptionModel
+}
+
+type DataSourceAll struct {
+	ID         types.String                                                 `tfsdk:"id"`
+	StreamID   types.String                                                 `tfsdk:"stream_id"`
+	Pagination fwtypes.ObjectValueOf[PaginationModel]                       `tfsdk:"pagination"`
+	Data       fwtypes.ListNestedObjectValueOf[BaseStreamSubscriptionModel] `tfsdk:"data"`
+}
+
+type PaginationModel struct {
+	Offset   types.Int32  `tfsdk:"offset"`
+	Limit    types.Int32  `tfsdk:"limit"`
+	Total    types.Int32  `tfsdk:"total"`
+	Next     types.String `tfsdk:"next"`
+	Previous types.String `tfsdk:"previous"`
+}
+
+type ResourceModel struct {
+	ID       types.String   `tfsdk:"id"`
+	Timeouts timeouts.Value `tfsdk:"timeouts"`
+	BaseStreamSubscriptionModel
+}
+
+type BaseStreamSubscriptionModel struct {
+	Type           types.String                                 `tfsdk:"type"`
+	Name           types.String                                 `tfsdk:"name"`
+	Description    types.String                                 `tfsdk:"description"`
+	Enabled        types.Bool                                   `tfsdk:"enabled"`
+	Filters        fwtypes.ListNestedObjectValueOf[FilterModel] `tfsdk:"filters"`         // List of filters
+	MetricSelector fwtypes.ObjectValueOf[SelectorModel]         `tfsdk:"metric_selector"` // Object of MetricSelectorModel
+	EventSelector  fwtypes.ObjectValueOf[SelectorModel]         `tfsdk:"event_selector"`  // Object of EventSelectorModel
+	Sink           fwtypes.ObjectValueOf[SinkModel]             `tfsdk:"sink"`            // Object of SinkModel
+	Href           types.String                                 `tfsdk:"href"`
+	Uuid           types.String                                 `tfsdk:"uuid"`
+	State          types.String                                 `tfsdk:"state"`
+	ChangeLog      fwtypes.ObjectValueOf[ChangeLogModel]        `tfsdk:"change_log"` // Object of ChangeLogModel
+}
+
+type FilterModel struct {
+	Property types.String                      `tfsdk:"property"`
+	Operator types.String                      `tfsdk:"operator"`
+	Values   fwtypes.ListValueOf[types.String] `tfsdk:"values"`
+	Or       types.Bool                        `tfsdk:"or"`
+}
+
+type SelectorModel struct {
+	Include fwtypes.ListValueOf[types.String] `tfsdk:"include"`
+	Except  fwtypes.ListValueOf[types.String] `tfsdk:"except"`
+}
+
+type SinkModel struct {
+	Uri              types.String                                       `tfsdk:"uri"`
+	Type             types.String                                       `tfsdk:"type"`
+	BatchEnabled     types.Bool                                         `tfsdk:"batch_enabled"`
+	BatchSizeMax     types.Int32                                        `tfsdk:"batch_size_max"`
+	BatchWaitTimeMax types.Int32                                        `tfsdk:"batch_wait_time_max"`
+	Host             types.String                                       `tfsdk:"host"`
+	Credential       fwtypes.ListNestedObjectValueOf[CredentialModel]   `tfsdk:"credential"` // Object of CredentialModel
+	Settings         fwtypes.ListNestedObjectValueOf[SinkSettingsModel] `tfsdk:"settings"`   // object of SinkSettingsModel
+}
+
+type CredentialModel struct {
+	Type           types.String `tfsdk:"type"`
+	AccessToken    types.String `tfsdk:"access_token"`
+	IntegrationKey types.String `tfsdk:"integration_key"`
+	ApiKey         types.String `tfsdk:"api_key"`
+	Username       types.String `tfsdk:"username"`
+	Password       types.String `tfsdk:"password"`
+}
+
+type SinkSettingsModel struct {
+	EventIndex      types.String `tfsdk:"event_index"`
+	MetricIndex     types.String `tfsdk:"metric_index"`
+	Source          types.String `tfsdk:"source"`
+	ApplicationKey  types.String `tfsdk:"application_key"`
+	EventUri        types.String `tfsdk:"event_uri"`
+	MetricUri       types.String `tfsdk:"metric_uri"`
+	TransformAlerts types.Bool   `tfsdk:"transform_alerts"`
+}
+
+type ChangeLogModel struct {
+	CreatedBy         types.String `tfsdk:"created_by"`
+	CreatedByFullName types.String `tfsdk:"created_by_full_name"`
+	CreatedByEmail    types.String `tfsdk:"created_by_email"`
+	CreatedDateTime   types.String `tfsdk:"created_date_time"`
+	UpdatedBy         types.String `tfsdk:"updated_by"`
+	UpdatedByFullName types.String `tfsdk:"updated_by_full_name"`
+	UpdatedByEmail    types.String `tfsdk:"updated_by_email"`
+	UpdatedDateTime   types.String `tfsdk:"updated_date_time"`
+	DeletedBy         types.String `tfsdk:"deleted_by"`
+	DeletedByFullName types.String `tfsdk:"deleted_by_full_name"`
+	DeletedByEmail    types.String `tfsdk:"deleted_by_email"`
+	DeletedDateTime   types.String `tfsdk:"deleted_date_time"`
+}
+
+func (m *DataSourceByIdsModel) parse(ctx context.Context, streamSubscription *fabricv4.StreamSubscription) diag.Diagnostics {
+	m.StreamID = types.StringValue(streamSubscription.GetUuid())
+	m.SubscriptionID = types.StringValue(streamSubscription.GetUuid())
+	m.ID = types.StringValue(streamSubscription.GetUuid())
+
+	diags := parseStreamSubscription(ctx, streamSubscription,
+		&m.Type,
+		&m.Name,
+		&m.Description,
+		&m.Enabled,
+		&m.Filters,
+		&m.MetricSelector,
+		&m.EventSelector,
+		&m.Sink,
+		&m.Href,
+		&m.Uuid,
+		&m.State,
+		&m.ChangeLog)
+	if diags.HasError() {
+		return diags
+	}
+
+	return diags
+}
+
+func (m *DataSourceAll) parse(ctx context.Context, streamSubscriptionsResponse *fabricv4.GetAllStreamSubscriptionResponse) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	if len(streamSubscriptionsResponse.GetData()) < 1 {
+		diags.AddError("no data retrieved by streamSubscription subscriptions data source",
+			"either the account does not have any streamSubscription subscription data to pull or the combination of limit and offset needs to be updated")
+		return diags
+	}
+
+	data := make([]BaseStreamSubscriptionModel, len(streamSubscriptionsResponse.GetData()))
+	streamSubscriptions := streamSubscriptionsResponse.GetData()
+	for index, streamSubscription := range streamSubscriptions {
+		var streamSubscriptionModel BaseStreamSubscriptionModel
+		diags = streamSubscriptionModel.parse(ctx, &streamSubscription)
+		if diags.HasError() {
+			return diags
+		}
+		data[index] = streamSubscriptionModel
+	}
+	responsePagination := streamSubscriptionsResponse.GetPagination()
+	pagination := PaginationModel{
+		Offset:   types.Int32Value(responsePagination.GetOffset()),
+		Limit:    types.Int32Value(responsePagination.GetLimit()),
+		Total:    types.Int32Value(responsePagination.GetTotal()),
+		Next:     types.StringValue(responsePagination.GetNext()),
+		Previous: types.StringValue(responsePagination.GetPrevious()),
+	}
+
+	m.ID = types.StringValue(data[0].Uuid.ValueString())
+	m.StreamID = types.StringValue(data[0].Uuid.ValueString())
+	m.Pagination = fwtypes.NewObjectValueOf[PaginationModel](ctx, &pagination)
+	m.Data = fwtypes.NewListNestedObjectValueOfValueSlice[BaseStreamSubscriptionModel](ctx, data)
+
+	return diags
+}
+
+func (m *ResourceModel) parse(ctx context.Context, streamSubscription *fabricv4.StreamSubscription) diag.Diagnostics {
+	m.ID = types.StringValue(streamSubscription.GetUuid())
+
+	diags := parseStreamSubscription(ctx, streamSubscription,
+		&m.Type,
+		&m.Name,
+		&m.Description,
+		&m.Enabled,
+		&m.Filters,
+		&m.MetricSelector,
+		&m.EventSelector,
+		&m.Sink,
+		&m.Href,
+		&m.Uuid,
+		&m.State,
+		&m.ChangeLog)
+	if diags.HasError() {
+		return diags
+	}
+
+	return diags
+}
+
+func (m *BaseStreamSubscriptionModel) parse(ctx context.Context, streamSubscription *fabricv4.StreamSubscription) diag.Diagnostics {
+	diags := parseStreamSubscription(ctx, streamSubscription,
+		&m.Type,
+		&m.Name,
+		&m.Description,
+		&m.Enabled,
+		&m.Filters,
+		&m.MetricSelector,
+		&m.EventSelector,
+		&m.Sink,
+		&m.Href,
+		&m.Uuid,
+		&m.State,
+		&m.ChangeLog)
+	if diags.HasError() {
+		return diags
+	}
+
+	return diags
+}
+
+func parseStreamSubscription(ctx context.Context, streamSubscription *fabricv4.StreamSubscription,
+	type_, name, description *basetypes.StringValue,
+	enabled *basetypes.BoolValue,
+	filters *fwtypes.ListNestedObjectValueOf[FilterModel],
+	metricSelector, eventSelector *fwtypes.ObjectValueOf[SelectorModel],
+	sink *fwtypes.ObjectValueOf[SinkModel],
+	href, uuid, state *basetypes.StringValue,
+	changeLog *fwtypes.ObjectValueOf[ChangeLogModel]) diag.Diagnostics {
+
+	var diags diag.Diagnostics
+
+	*type_ = types.StringValue(string(streamSubscription.GetType()))
+	*name = types.StringValue(streamSubscription.GetName())
+	*description = types.StringValue(streamSubscription.GetDescription())
+	*href = types.StringValue(streamSubscription.GetHref())
+	*uuid = types.StringValue(streamSubscription.GetUuid())
+	*state = types.StringValue(string(streamSubscription.GetState()))
+	*enabled = types.BoolValue(streamSubscription.GetEnabled())
+
+	// Parse filters
+	streamSubscriptionFilters := streamSubscription.GetFilters()
+	filterModels := make([]FilterModel, len(streamSubscriptionFilters.GetAnd()))
+	for i, filter := range streamSubscriptionFilters.GetAnd() {
+		if len(filter.StreamFilterOrFilter.GetOr()) > 0 {
+			for j, orFilter := range filter.StreamFilterOrFilter.GetOr() {
+				orFilterModel, diags := parseSimpleExpression(ctx, &orFilter, true)
+				if diags.HasError() {
+					return diags
+				}
+				if j == 0 {
+					filterModels[i] = orFilterModel
+				} else {
+					filterModels = append(filterModels, orFilterModel)
+				}
+			}
+		} else {
+			filterModels[i], diags = parseSimpleExpression(ctx, filter.StreamFilterSimpleExpression, false)
+			if diags.HasError() {
+				return diags
+			}
+		}
+	}
+	*filters = fwtypes.NewListNestedObjectValueOfValueSlice[FilterModel](ctx, filterModels)
+
+	// Parse MetricSelector
+	metricSelectorObject, diags := parseSelectorModel(ctx, streamSubscription.GetMetricSelector())
+	if diags.HasError() {
+		return diags
+	}
+	*metricSelector = metricSelectorObject
+
+	// Parse EventSelector
+	eventSelectorObject, diags := parseSelectorModel(ctx, streamSubscription.GetEventSelector())
+	if diags.HasError() {
+		return diags
+	}
+	*eventSelector = eventSelectorObject
+
+	// Parse Sink
+	streamSubSink := streamSubscription.GetSink()
+	sinkModel := SinkModel{
+		Uri:              types.StringValue(streamSubSink.GetUri()),
+		Type:             types.StringValue(string(streamSubSink.GetType())),
+		BatchEnabled:     types.BoolValue(streamSubSink.GetBatchEnabled()),
+		BatchSizeMax:     types.Int32Value(streamSubSink.GetBatchSizeMax()),
+		BatchWaitTimeMax: types.Int32Value(streamSubSink.GetBatchWaitTimeMax()),
+		Host:             types.StringValue(streamSubSink.GetHost()),
+	}
+	*sink = fwtypes.NewObjectValueOf[SinkModel](ctx, &sinkModel)
+
+	// Parse ChangeLog
+	streamSubscriptionChangeLog := streamSubscription.GetChangeLog()
+	changeLogModel := ChangeLogModel{
+		CreatedBy:         types.StringValue(streamSubscriptionChangeLog.GetCreatedBy()),
+		CreatedByFullName: types.StringValue(streamSubscriptionChangeLog.GetCreatedByFullName()),
+		CreatedByEmail:    types.StringValue(streamSubscriptionChangeLog.GetCreatedByEmail()),
+		CreatedDateTime:   types.StringValue(streamSubscriptionChangeLog.GetCreatedDateTime().Format(fabric.TimeFormat)),
+		UpdatedBy:         types.StringValue(streamSubscriptionChangeLog.GetUpdatedBy()),
+		UpdatedByFullName: types.StringValue(streamSubscriptionChangeLog.GetUpdatedByFullName()),
+		UpdatedByEmail:    types.StringValue(streamSubscriptionChangeLog.GetUpdatedByEmail()),
+		UpdatedDateTime:   types.StringValue(streamSubscriptionChangeLog.GetUpdatedDateTime().Format(fabric.TimeFormat)),
+		DeletedBy:         types.StringValue(streamSubscriptionChangeLog.GetDeletedBy()),
+		DeletedByFullName: types.StringValue(streamSubscriptionChangeLog.GetDeletedByFullName()),
+		DeletedByEmail:    types.StringValue(streamSubscriptionChangeLog.GetDeletedByEmail()),
+		DeletedDateTime:   types.StringValue(streamSubscriptionChangeLog.GetDeletedDateTime().Format(fabric.TimeFormat)),
+	}
+	*changeLog = fwtypes.NewObjectValueOf[ChangeLogModel](ctx, &changeLogModel)
+
+	return diags
+}
+
+func parseSimpleExpression(ctx context.Context, expression *fabricv4.StreamFilterSimpleExpression, orGroup bool) (FilterModel, diag.Diagnostics) {
+	values := int_fw.StringSliceToAttrValue(expression.GetValues())
+	fwValues, diags := fwtypes.NewListValueOf[types.String](ctx, values)
+	if diags.HasError() {
+		return FilterModel{}, diags
+	}
+	return FilterModel{
+		Property: types.StringValue(expression.GetProperty()),
+		Operator: types.StringValue(expression.GetOperator()),
+		Values:   fwValues,
+		Or:       types.BoolValue(orGroup),
+	}, nil
+}
+
+func parseSelectorModel(ctx context.Context, selector fabricv4.StreamSubscriptionSelector) (fwtypes.ObjectValueOf[SelectorModel], diag.Diagnostics) {
+	var diags diag.Diagnostics
+	inclusions, diags := fwtypes.NewListValueOf[types.String](ctx, int_fw.StringSliceToAttrValue(selector.GetInclude()))
+	if diags.HasError() {
+		return fwtypes.NewObjectValueOfNull[SelectorModel](ctx), diags
+	}
+	exclusions, diags := fwtypes.NewListValueOf[types.String](ctx, int_fw.StringSliceToAttrValue(selector.GetExcept()))
+	if diags.HasError() {
+		return fwtypes.NewObjectValueOfNull[SelectorModel](ctx), diags
+	}
+	selectorModel := SelectorModel{
+		Include: inclusions,
+		Except:  exclusions,
+	}
+	return fwtypes.NewObjectValueOf[SelectorModel](ctx, &selectorModel), diags
+}
--- internal/resources/fabric/stream_subscription/resource_schema.go
+++ internal/resources/fabric/stream_subscription/resource_schema.go
@@ -2,6 +2,10 @@ package stream_subscription
 
 import (
 	"context"
+
+	"github.com/equinix/terraform-provider-equinix/internal/framework"
+	fwtypes "github.com/equinix/terraform-provider-equinix/internal/framework/types"
+
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -14,6 +18,7 @@ Additional Documentation:
 * Getting Started: https://docs.equinix.com/en-us/Content/KnowledgeCenter/Fabric/GettingStarted/Integrating-with-Fabric-V4-APIs/IntegrateWithSink.htm
 * API: https://developer.equinix.com/catalog/fabricv4#tag/Stream-Subscriptions`,
 		Attributes: map[string]schema.Attribute{
+			"id": framework.IDAttributeDefaultDescription(),
 			"type": schema.StringAttribute{
 				Description: "Type of the stream subscription request",
 				Required:    true,
@@ -32,25 +37,25 @@ Additional Documentation:
 			},
 			"filters": schema.ListNestedAttribute{
 				Description: "List of filters to apply to the stream subscription selectors. Maximum of 8. All will be AND'd together with 1 of the 8 being a possible OR group of 3",
-				Optional: true,
+				Optional:    true,
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"property": schema.StringAttribute{
 							Description: "Property to apply the filter to",
-							Required: true,
+							Required:    true,
 						},
 						"operator": schema.StringAttribute{
 							Description: "Operation applied to the values of the filter",
-							Required: true,
-						}
+							Required:    true,
+						},
 						"values": schema.ListAttribute{
 							Description: "List of values to apply the operation to for the specified property",
-							Required: true,
+							Required:    true,
 							ElementType: types.StringType,
 						},
 						"or": schema.BoolAttribute{
 							Description: "Boolean value to specify if this filter is a part of the OR group. Has a maximum of 3 and only counts for 1 of the 8 possible filters",
-							Optional: true,
+							Optional:    true,
 						},
 					},
 				},
@@ -88,7 +93,158 @@ Additional Documentation:
 				},
 			},
 			"sink": schema.SingleNestedAttribute{
-				
+				Description: "The details of the subscriber to the Equinix Stream",
+				Required:    true,
+				Attributes: map[string]schema.Attribute{
+					"uri": schema.StringAttribute{
+						Description: "Publicly reachable http endpoint destination for data stream",
+						Optional:    true,
+					},
+					"type": schema.StringAttribute{
+						Description: "Type of the subscriber",
+						Required:    true,
+					},
+					"batch_enabled": schema.BoolAttribute{
+						Description: "Boolean switch enabling batch delivery of data",
+						Optional:    true,
+					},
+					"batch_size_max": schema.Int32Attribute{
+						Description: "Maximum size of the batch delivery if enabled",
+						Optional:    true,
+					},
+					"batch_wait_time_max": schema.Int32Attribute{
+						Description: "Maximum time to wait for batch delivery if enabled",
+						Optional:    true,
+					},
+					"host": schema.StringAttribute{
+						Description: "Known hostname of certain data stream subscription products. Not to be confused with a variable URI",
+						Optional:    true,
+					},
+					"credential": schema.SingleNestedAttribute{
+						Description: "Access details for the specified sink type",
+						Required:    true,
+						Attributes: map[string]schema.Attribute{
+							"type": schema.StringAttribute{
+								Description: "Type of the credential being passed",
+								Required:    true,
+							},
+							"access_token": schema.StringAttribute{
+								Description: "Passed as Authorization header value",
+								Optional:    true,
+							},
+							"integration_key": schema.StringAttribute{
+								Description: "Passed as Authorization header value",
+								Optional:    true,
+							},
+							"api_key": schema.StringAttribute{
+								Description: "Passed as Authorization header value",
+								Optional:    true,
+							},
+							"username": schema.StringAttribute{
+								Description: "Passed as Authorization header value",
+								Optional:    true,
+							},
+							"password": schema.StringAttribute{
+								Description: "Passed as Authorization header value",
+								Optional:    true,
+							},
+						},
+					},
+					"settings": schema.SingleNestedAttribute{
+						Description: "Stream subscription sink settings",
+						Optional:    true,
+						Attributes: map[string]schema.Attribute{
+							"event_index": schema.StringAttribute{
+								Optional: true,
+							},
+							"metric_index": schema.StringAttribute{
+								Optional: true,
+							},
+							"source": schema.StringAttribute{
+								Optional: true,
+							},
+							"application_key": schema.StringAttribute{
+								Optional: true,
+							},
+							"event_uri": schema.StringAttribute{
+								Optional: true,
+							},
+							"metric_uri": schema.StringAttribute{
+								Optional: true,
+							},
+							"transform_alerts": schema.BoolAttribute{
+								Optional: true,
+							},
+						},
+					},
+				},
+			},
+			"href": schema.StringAttribute{
+				Description: "Equinix assigned URI of the stream subscription resource",
+				Computed:    true,
+			},
+			"uuid": schema.StringAttribute{
+				Description: "Equinix assigned unique identifier of the stream subscription resource",
+				Computed:    true,
+			},
+			"state": schema.StringAttribute{
+				Description: "Value representing provisioning status for the stream resource",
+				Computed:    true,
+			},
+			"change_log": schema.SingleNestedAttribute{
+				Description: "Details of the last change on the stream resource",
+				Computed:    true,
+				CustomType:  fwtypes.NewObjectTypeOf[ChangeLogModel](ctx),
+				Attributes: map[string]schema.Attribute{
+					"created_by": schema.StringAttribute{
+						Description: "User name of creator of the stream resource",
+						Computed:    true,
+					},
+					"created_by_full_name": schema.StringAttribute{
+						Description: "Legal name of creator of the stream resource",
+						Computed:    true,
+					},
+					"created_by_email": schema.StringAttribute{
+						Description: "Email of creator of the stream resource",
+						Computed:    true,
+					},
+					"created_date_time": schema.StringAttribute{
+						Description: "Creation time of the stream resource",
+						Computed:    true,
+					},
+					"updated_by": schema.StringAttribute{
+						Description: "User name of last updater of the stream resource",
+						Computed:    true,
+					},
+					"updated_by_full_name": schema.StringAttribute{
+						Description: "Legal name of last updater of the stream resource",
+						Computed:    true,
+					},
+					"updated_by_email": schema.StringAttribute{
+						Description: "Email of last updater of the stream resource",
+						Computed:    true,
+					},
+					"updated_date_time": schema.StringAttribute{
+						Description: "Last update time of the stream resource",
+						Computed:    true,
+					},
+					"deleted_by": schema.StringAttribute{
+						Description: "User name of deleter of the stream resource",
+						Computed:    true,
+					},
+					"deleted_by_full_name": schema.StringAttribute{
+						Description: "Legal name of deleter of the stream resource",
+						Computed:    true,
+					},
+					"deleted_by_email": schema.StringAttribute{
+						Description: "Email of deleter of the stream resource",
+						Computed:    true,
+					},
+					"deleted_date_time": schema.StringAttribute{
+						Description: "Deletion time of the stream resource",
+						Computed:    true,
+					},
+				},
 			},
 		},
 	}
