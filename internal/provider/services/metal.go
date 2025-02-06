0aa69642df5d94ddd78d438616cc5ea492c481f2
Charles Treatman <ctreatman@equinix.com>
2025-02-06 09:15:17 -0600

    rename project SSH key directory to match package name

--- internal/provider/services/metal.go
+++ internal/provider/services/metal.go
@@ -6,7 +6,7 @@ import (
 	metalorganization "github.com/equinix/terraform-provider-equinix/internal/resources/metal/organization"
 	metalorganizationmember "github.com/equinix/terraform-provider-equinix/internal/resources/metal/organization_member"
 	metalproject "github.com/equinix/terraform-provider-equinix/internal/resources/metal/project"
-	metalprojectsshkey "github.com/equinix/terraform-provider-equinix/internal/resources/metal/project_ssh_key"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/projectsshkey"
 	metalsshkey "github.com/equinix/terraform-provider-equinix/internal/resources/metal/ssh_key"
 	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/vlan"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
@@ -17,7 +17,7 @@ func MetalResources() []func() resource.Resource {
 	return []func() resource.Resource{
 		metalgateway.NewResource,
 		metalproject.NewResource,
-		metalprojectsshkey.NewResource,
+		projectsshkey.NewResource,
 		metalsshkey.NewResource,
 		metalconnection.NewResource,
 		metalorganization.NewResource,
@@ -30,7 +30,7 @@ func MetalDatasources() []func() datasource.DataSource {
 	return []func() datasource.DataSource{
 		metalgateway.NewDataSource,
 		metalproject.NewDataSource,
-		metalprojectsshkey.NewDataSource,
+		projectsshkey.NewDataSource,
 		metalconnection.NewDataSource,
 		metalorganization.NewDataSource,
 		vlan.NewDataSource,
