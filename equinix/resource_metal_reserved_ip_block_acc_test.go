ed852bcaecefc49c6c1efcbb7ab14a42a10bdf2a
Charles Treatman <ctreatman@equinix.com>
2025-02-19 11:48:33 -0600

    chore(ci): remove tests that rely on facility deployment (#857)
    Facility deployment is disabled for all customers and only used in
    tests.

--- equinix/resource_metal_reserved_ip_block_acc_test.go
+++ equinix/resource_metal_reserved_ip_block_acc_test.go
@@ -3,7 +3,6 @@ package equinix
 import (
 	"bytes"
 	"fmt"
-	"regexp"
 	"testing"
 	"text/template"
 
@@ -13,8 +12,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
-
-var matchIpBlockErrTimeout = regexp.MustCompile(".* timeout while waiting for state to become 'created'.*")
 
 func testAccMetalReservedIPBlockConfig_global(name string) string {
 	return fmt.Sprintf(`
@@ -31,30 +28,6 @@ resource "equinix_metal_reserved_ip_block" "test" {
 		"foo": "bar"
 	})
 }`, name, name)
-}
-
-func testAccMetalReservedIPBlockConfig_public(name, createTimeout string) string {
-	if createTimeout == "" {
-		createTimeout = "20m"
-	}
-
-	return fmt.Sprintf(`
-resource "equinix_metal_project" "foobar" {
-	name = "tfacc-reserved_ip_block-%s"
-}
-
-resource "equinix_metal_reserved_ip_block" "test" {
-	project_id  = equinix_metal_project.foobar.id
-	facility    = "ny5"
-	type        = "public_ipv4"
-	description = "tfacc-reserved_ip_block-%s"
-	quantity    = 2
-	tags        = ["Tag1", "Tag2"]
-
-    timeouts {
-      create = "%s"
-    }
-}`, name, name, createTimeout)
 }
 
 // testAccMetalReservedIPBlockConfig_metro generates a config for a metro IP
@@ -117,40 +90,6 @@ func TestAccMetalReservedIPBlock_global(t *testing.T) {
 	})
 }
 
-func TestAccMetalReservedIPBlock_public(t *testing.T) {
-	rs := acctest.RandString(10)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ExternalProviders:        testExternalProviders,
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		CheckDestroy:             testAccMetalReservedIPBlockCheckDestroyed,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccMetalReservedIPBlockConfig_public(rs, ""),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "facility", "ny5"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "type", "public_ipv4"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "quantity", "2"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "netmask", "255.255.255.254"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "public", "true"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "management", "false"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "tags.#", "2"),
-					resource.TestCheckResourceAttrSet("equinix_metal_reserved_ip_block.test", "network"),
-					resource.TestCheckResourceAttrSet("equinix_metal_reserved_ip_block.test", "cidr"),
-				),
-			},
-		},
-	})
-}
-
 func TestAccMetalReservedIPBlock_metro(t *testing.T) {
 	rs := acctest.RandString(10)
 	tag := "tag"
@@ -191,28 +130,6 @@ func TestAccMetalReservedIPBlock_metro(t *testing.T) {
 	})
 }
 
-func TestAccMetalReservedIPBlock_importBasic(t *testing.T) {
-	rs := acctest.RandString(10)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ExternalProviders:        testExternalProviders,
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		CheckDestroy:             testAccMetalReservedIPBlockCheckDestroyed,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccMetalReservedIPBlockConfig_public(rs, ""),
-			},
-			{
-				ResourceName:            "equinix_metal_reserved_ip_block.test",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"wait_for_state"},
-			},
-		},
-	})
-}
-
 func testAccMetalReservedIPBlockCheckDestroyed(s *terraform.State) error {
 	client := testAccProvider.Meta().(*config.Config).Metal
 
@@ -228,49 +145,6 @@ func testAccMetalReservedIPBlockCheckDestroyed(s *terraform.State) error {
 	return nil
 }
 
-func testAccMetalReservedIPBlockConfig_facilityToMetro(line string) string {
-	return fmt.Sprintf(`
-resource "equinix_metal_project" "foobar" {
-	name = "tfacc-reserved_ip_block_fac_met_test"
-}
-
-resource "equinix_metal_reserved_ip_block" "test" {
-	project_id  = equinix_metal_project.foobar.id
-	%s
-	type        = "public_ipv4"
-	quantity    = 2
-	tags        = ["Tag1", "Tag2"]
-}`, line)
-}
-
-func TestAccMetalReservedIPBlock_facilityToMetro(t *testing.T) {
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ExternalProviders:        testExternalProviders,
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		CheckDestroy:             testAccMetalReservedIPBlockCheckDestroyed,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccMetalReservedIPBlockConfig_facilityToMetro(`   facility = "ny5"`),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "facility", "ny5"),
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "metro", "ny"),
-				),
-			},
-			{
-				Config: testAccMetalReservedIPBlockConfig_facilityToMetro(`   metro = "ny"`),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"equinix_metal_reserved_ip_block.test", "metro", "ny"),
-				),
-				PlanOnly: true,
-			},
-		},
-	})
-}
-
 func testAccMetalReservedIP_device(name string) string {
 	return fmt.Sprintf(`
 %s
@@ -281,7 +155,7 @@ resource "equinix_metal_project" "foobar" {
 
 resource "equinix_metal_reserved_ip_block" "test" {
 	project_id  = equinix_metal_project.foobar.id
-	facility    = local.facility
+	metro       = local.metro
 	type        = "public_ipv4"
 	quantity    = 2
 }
@@ -289,7 +163,7 @@ resource "equinix_metal_reserved_ip_block" "test" {
 resource "equinix_metal_device" "test" {
   project_id       = equinix_metal_project.foobar.id
   plan             = local.plan
-  facilities       = [local.facility]
+  metro            = local.metro
   operating_system = local.os
   hostname         = "tfacc-reserved-ip-device"
   billing_cycle    = "hourly"
@@ -323,23 +197,6 @@ func TestAccMetalReservedIPBlock_device(t *testing.T) {
 						"equinix_metal_device.test", "network.0.gateway",
 					),
 				),
-			},
-		},
-	})
-}
-
-func TestAccMetalReservedIPBlockCreate_public_timeout(t *testing.T) {
-	rs := acctest.RandString(10)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ExternalProviders:        testExternalProviders,
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		CheckDestroy:             testAccMetalReservedIPBlockCheckDestroyed,
-		Steps: []resource.TestStep{
-			{
-				Config:      testAccMetalReservedIPBlockConfig_public(rs, "2s"),
-				ExpectError: matchIpBlockErrTimeout,
 			},
 		},
 	})
