6fbc235ec0cb5d826639eac39a3481040961f56a
Charles Treatman <ctreatman@equinix.com>
2025-06-27 10:11:29 -0500

    chore: enable code review by service owners for test sweepers (#909)
    This replicates the approach taken for provider resource and data source
    registration in #797 and applies it to test sweeper registration so that
    service teams can add and remove test sweepers for their own resources
    as needed without waiting for review from a cross-cutting team.

---
+++ internal/sweep/services/metal.go
@@ -0,0 +1,27 @@
+// Package services for registering service-specific test sweepers
+package services
+
+import (
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/connection"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/device"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/organization"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/project"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/ssh_key"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/user_api_key"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/virtual_circuit"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/vlan"
+	"github.com/equinix/terraform-provider-equinix/internal/resources/metal/vrf"
+)
+
+// AddMetalTestSweepers registers test sweepers for Fabric resources
+func AddMetalTestSweepers() {
+	connection.AddTestSweeper()
+	device.AddTestSweeper()
+	organization.AddTestSweeper()
+	project.AddTestSweeper()
+	ssh_key.AddTestSweeper()
+	user_api_key.AddTestSweeper()
+	virtual_circuit.AddTestSweeper()
+	vlan.AddTestSweeper()
+	vrf.AddTestSweeper()
+}
