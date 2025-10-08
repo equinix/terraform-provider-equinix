bdc71abccfd06c27fe9d1b3f2395630dcaa364f0
kpdhulipala <84343462+kpdhulipala@users.noreply.github.com>
2025-10-08 09:54:50 -0700

    feat: Purchase Order number sending part of  Secondary device in Create HA device request (#963)
    feat: Purchase Order number sending part of Secondary device in Create
    HA device request and added few tests for coverage.

---
+++ examples/resources/equinix_network_device/aruba_edgeconnect_ha_device_wth_purchase_order.tf
@@ -0,0 +1,45 @@
+# Create Aruba Edgeconnect SDWAN HA device with 2different account numbers with purchase orders
+
+data "equinix_network_account" "sv" {
+  metro_code = "SV"
+}
+
+
+resource "equinix_network_device" "ARUBA-EDGECONNECT-AM" {
+  name                  = "TF_Aruba_Edge_Connect"
+  project_id            = "XXXXX"
+  metro_code            = data.equinix_network_account.sv.metro_code
+  type_code             = "EDGECONNECT-SDWAN"
+  self_managed          = true
+  byol                  = true
+  package_code          = "EC-V"
+  notifications         = ["test@eq.com"]
+  account_number        = data.equinix_network_account.sv.number
+  version               = "9.4.2.3"
+  core_count            = 2
+  term_length           = 1
+  additional_bandwidth  = 50
+  interface_count       = 32
+  acl_template_id       = "XXXXXXX"
+  purchase_order_number = "PO-Primary-Account-123"
+  vendor_configuration  = {
+    accountKey : "xxxxx"
+    accountName : "xxxx"
+    applianceTag : "tests"
+    hostname : "test"
+  }
+  secondary_device {
+    name                  = "TF_CHECKPOINT"
+    metro_code            = data.equinix_network_account.sv.metro_code
+    account_number        = data.equinix_network_account.sv.number
+    purchase_order_number = "PO-Secondary-Account-123"
+    acl_template_id       = "XXXXXXX"
+    notifications         = ["test@eq.com"]
+    vendor_configuration  = {
+      accountKey : "xxxxx"
+      accountName : "xxxx"
+      applianceTag : "test"
+      hostname : "test"
+    }
+  }
+}
