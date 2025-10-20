e5ddbdb24e99f1fd705f83fc3952effe7cc59034
Adam Mohammed <admohammed@equinix.com>
2025-10-20 12:34:34 -0400

    remove isVlanEqual helper and use path.Base instead

--- internal/resources/metal/vlan/resource.go
+++ internal/resources/metal/vlan/resource.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"path"
 	"strings"
 
 	"github.com/equinix/equinix-sdk-go/services/metalv1"
@@ -255,7 +256,7 @@ func (r *Resource) Delete(ctx context.Context, request resource.DeleteRequest, r
 	for _, instance := range vlan.Instances {
 		for _, port := range instance.NetworkPorts {
 			for _, v := range port.AttachedVirtualNetworks {
-				if isVlanEqual(vlan.ID, v) {
+				if path.Base(v.Href) == vlan.ID {
 					_, resp, err = client.Ports.Unassign(port.ID, vlan.ID)
 					if equinix_errors.IgnoreResponseErrors(equinix_errors.HttpForbidden, equinix_errors.HttpNotFound)(resp, err) != nil {
 						response.Diagnostics.AddError("Error unassign port with Vlan",
