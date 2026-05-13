60c86231834331c6186781bd46f218dec647dad9
kpdhulipala <84343462+kpdhulipala@users.noreply.github.com>
2026-05-13 13:18:17 -0700

    feat: examples for creating a ha device - 6WIND VSR (#1017)
    feat: examples for creating a ha device - 6WIND VSR

---
+++ examples/resources/equinix_network_device/6wind_vsr_ha_device.tf
@@ -0,0 +1,39 @@
+# Create 6WIND VSR HA device
+
+data "equinix_network_account" "sv" {
+  metro_code = "SV"
+}
+
+resource "equinix_network_device" "six-wind-vsr" {
+  name            = "6WIND-VSR"
+  project_id      = "xxxxxxx"
+  metro_code      = data.equinix_network_account.sv.metro_code
+  type_code       = "6WIND-VSR"
+  self_managed    = true
+  byol            = true
+  interface_count = 10
+  package_code    = "STD"
+  notifications   = ["test@eq.com"]
+  account_number  = data.equinix_network_account.sv.number
+  version         = "3.10.8"
+  core_count      = 2
+  term_length     = 1
+  vendor_configuration = {
+    hostname = "test"
+    token    = "xxxx"
+  }
+  ssh_key {
+    username = "xxxx"
+    key_name = "xxxxx"
+  }
+  secondary_device {
+    name           = "6WIND-VSR-Sec"
+    metro_code     = data.equinix_network_account.sv.metro_code
+    account_number = data.equinix_network_account.sv.number
+    notifications  = ["test@eq.com"]
+    vendor_configuration = {
+      hostname = "test"
+      token    = "xxxx"
+    }
+  }
+}
