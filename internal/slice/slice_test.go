ca33ed87ab10d279050447b114367eb7bf74a39f
Ng Tek In <tng1@ap.equinix.com>
2026-06-26 14:59:07 +0800

    Update slice and test case

--- internal/slice/slice_test.go
+++ internal/slice/slice_test.go
@@ -7,12 +7,33 @@ import (
 )
 
 func TestMap(t *testing.T) {
-	in := []int{1, 2, 3}
-	out := Map(in, func(n int) string { return strconv.Itoa(n + 3) })
+	t.Run("nil slice returns nil", func(t *testing.T) {
+		out := Map(nil, func(n int) string { return strconv.Itoa(n + 3) })
 
-	expected := []string{"4", "5", "6"}
+		if out != nil {
+			t.Errorf(`Output = %q, want nil`, out)
+		}
+	})
 
-	if !reflect.DeepEqual(out, expected) {
-		t.Errorf(`Output = %q, want match for %q`, out, expected)
-	}
+	t.Run("empty slice returns empty slice", func(t *testing.T) {
+		in := []int{}
+		out := Map(in, func(n int) string { return strconv.Itoa(n + 3) })
+
+		expected := []string{}
+
+		if !reflect.DeepEqual(out, expected) {
+			t.Errorf(`Output = %q, want match for %q`, out, expected)
+		}
+	})
+
+	t.Run("slice returns mapped slice", func(t *testing.T) {
+		in := []int{1, 2, 3}
+		out := Map(in, func(n int) string { return strconv.Itoa(n + 3) })
+
+		expected := []string{"4", "5", "6"}
+
+		if !reflect.DeepEqual(out, expected) {
+			t.Errorf(`Output = %q, want match for %q`, out, expected)
+		}
+	})
 }
