868d2d1a14f6852a0bee7d25a1dcd43f82fb7403
Ng Tek In <tng1@ap.equinix.com>
2026-07-03 16:17:32 +0800

    Implement ExpectKnownAttributesAt

--- internal/fabric/testing_helpers/check.go
+++ internal/fabric/testing_helpers/check.go
@@ -15,15 +15,32 @@ func ExpectKnownAttributes(address string, checks map[string]knownvalue.Check) e
 	}
 }
 
+func ExpectKnownAttributesAt(address string, path tfjsonpath.Path, checks map[string]knownvalue.Check) expectKnownAttributes {
+	return expectKnownAttributes{
+		address:  address,
+		checks:   checks,
+		basePath: &path,
+	}
+}
+
 var _ statecheck.StateCheck = expectKnownAttributes{}
 
 type expectKnownAttributes struct {
-	address string
-	checks  map[string]knownvalue.Check
+	address  string
+	checks   map[string]knownvalue.Check
+	basePath *tfjsonpath.Path
 }
 
 func (e expectKnownAttributes) CheckState(ctx context.Context, req statecheck.CheckStateRequest, res *statecheck.CheckStateResponse) {
-	for path, check := range e.checks {
-		statecheck.ExpectKnownValue(e.address, tfjsonpath.New(path), check).CheckState(ctx, req, res)
+	for key, check := range e.checks {
+		path := func() tfjsonpath.Path {
+			if e.basePath == nil {
+				return tfjsonpath.New(key)
+			}
+
+			return e.basePath.AtMapKey(key)
+		}()
+
+		statecheck.ExpectKnownValue(e.address, path, check).CheckState(ctx, req, res)
 	}
 }
